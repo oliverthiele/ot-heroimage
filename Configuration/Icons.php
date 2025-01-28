5b46c6d72037f24f9213a42777fdd0c83aa8945f
Oliver Thiele <mail@oliver-thiele.de>
2025-01-28 13:47:45 +0100

    [TASK] Update code for TYPO3 v13
    - Changed icon source for the hero image (free FontAwesome icon).
    - Added new plugin registration for the content element.
    - Updated language labels and descriptions for clarity.
    - Removed outdated TCA configuration and TSconfig file.
    - Bumped version number to 2.0.0 and updated TYPO3 dependency.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -5,6 +5,6 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     'ot-heroimage' => [
         'provider' => SvgIconProvider::class,
-        'source' => 'EXT:ot_heroimage/Resources/Public/Icons/FarImageLandscape.svg',
+        'source' => 'EXT:ot_heroimage/Resources/Public/Icons/FasPanorama.svg',
     ],
 ];
