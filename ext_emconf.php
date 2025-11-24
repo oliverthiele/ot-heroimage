71dcb2e5f42ef127fd31515635112edd1f6473d8
Oliver Thiele <mail@oliver-thiele.de>
2025-11-24 15:01:10 +0100

    [TASK] Remove default div-container and header
    - add php-cs-fixer and codesniffer cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ot_heroimage'] = [
     'title' => 'Heroimage',
     'description' => 'This content element can output larger image widths than normal elements and is therefore suitable for outputting images across the entire screen width.',
     'category' => 'plugin',
     'author' => 'Oliver Thiele',
     'author_email' => 'mail@oliver-thiele.de',
     'state' => 'stable',
-    'version' => '3.1.0',
+    'version' => '3.1.1',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-13.4.99',
